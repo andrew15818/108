c139279259747cfba4ae159bc70f6f526f9abe03
andrew15818 <andreseeponce@gmail.com>
2019-09-14 22:47:28 +0000

    how to attempt to find n, the upper bound for the input?

---
+++ fall/algorithms/homework/hw1/hw1-A.cpp
@@ -0,0 +1,18 @@
+//find the number of testcases possible in a given time
+//given the program takes n x floor(sqrt(n)) x floor(log2(n))
+#include<iostream>
+#include<cmath>
+using namespace std;
+//Let's see if this is equivalent to the membership problem? 
+long long int eqaution(long long int n){
+	return (
+}
+int main(){
+	int cases,seconds;
+	cin>>cases;
+
+	for(int i=0;i<cases;i++){	
+		cin>>seconds;
+	}
+	return  0;
+}
