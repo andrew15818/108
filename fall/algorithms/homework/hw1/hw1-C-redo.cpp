a7a46559f22024bcf80ec84660d1fddf01a5d1de
andrew15818 <andreseeponce@gmail.com>
2019-09-19 00:33:56 +0000

    I don't think my thinking is feasible for this one

--- fall/algorithms/homework/hw1/hw1-C-redo.cpp
+++ fall/algorithms/homework/hw1/hw1-C-redo.cpp
@@ -3,7 +3,7 @@ using namespace std;
 //in this approach just build the huge matrix and try
 //to only have opne big matrix instead of several 
 #define MAXSIZE 4096
-int field[MAXSIZE][MAXSIZE] /*, result[MAXSIZE][MAXSIZE], input[MAXSIZE][MAXSIZE]*/;
+int field[MAXSIZE][MAXSIZE] , result[MAXSIZE][MAXSIZE], input[MAXSIZE][MAXSIZE];
 
 
 void init(int count){
@@ -19,17 +19,17 @@ field[1][1] = -1;
 				field[j+lim][k] = field[j][k];
 				field[j+lim][k+lim] = -field[j][k];
 				//result[j][k] += (input[j][k] * field[j][k]);
-			}
-			/*if(i==count/2){
-
-			}*/
+				//if(i==count>>1){
+				//		long int intermediate = (field[]);
+				//}
+			}	
 		}
 	}
 }
 void print(int n){
 	for(int i=0; i<n;i++){
 		for(int j=0;j<n;j++){
-			cout<<field[i][j]<<" ";
+			cout<<result[i][j]<<" ";
 			if(j==n-1){cout<<endl;}
 		}
 	}
@@ -38,7 +38,7 @@ int main(){
 	int n;
 	cin>>n;
 	init(n);
-	long int input[n][n], result[n][n];
+	//long int input[n][n], result[n][n];
 	for(int i =0;i<n;i++){
 		for(int j=0; j<n; j++){
 			cin>>input[i][j];
@@ -56,21 +56,28 @@ int main(){
 		}
 	}
 */
+	
+	for(int i=0;i<lim;i++){
+		for(int j=0;j<lim;j++){
+			int intermediate = 0, interDown = 0, interRight = 0, interDiag = 0; // we have to calc values under, right, and diag
+			cout<<"for "<<i+lim<<","<<j<<endl;
+			for(int k=0; k<lim; k++){
+				intermediate += ((field[i][k] * input[k][j]) + (field[i+lim][k]*input[k+lim][j]));	
+				interDown+= ((field[i+lim][k] * input[k][j+lim]) + (field[i+lim][k+lim] * input[k+lim][j+lim]));
+				
+				cout<<"\t"<<field[i+lim][k]<<" * "<<input[k][j+lim]<<", "<<field[i+lim][k+lim]<<" * "<<input[k+lim][j+lim]<<endl;
 
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			int intermediate = 0;
-			for(int k=0; k<n; k++){
-				intermediate += (field[i][k] * input[k][j]);
-			
+				//interRight+=;
+				//interDiag+=;
 			}		
 			result[i][j] = intermediate; 
-			cout<<result[i][j]<<" ";
+			result[i+lim][j] = interDown;
+			//cout<<result[i][j]<<" ";
 			if(j==n-1){cout<<endl;}
 		}
 	}
 
-	//print(n);
+	print(n);
 	return 0;
 }
 
