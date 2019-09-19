5b50ca8cd6553e69a0e491c4ddc2b09ff5c50f77
andrew15818 <andreseeponce@gmail.com>
2019-09-19 23:07:21 +0000

    TLE on some testcases

--- fall/algorithms/homework/hw1/hw1-A.cpp
+++ fall/algorithms/homework/hw1/hw1-A.cpp
@@ -1,5 +1,6 @@
 //find the number of testcases possible in a given time
 //given the program takes n x floor(sqrt(n)) x floor(log2(n))
+//still TLE on 3 testcases
 #include<iostream>
 #include<cstdio>
 #include<cmath>
@@ -8,24 +9,20 @@
 using namespace std;
 //Let's see if this is equivalent to the membership problem? 
 double equation(const long long int& maxOps, const long long int& guess ){
-	//return ((n*OPS) / (guess*pow(guess,PWR )*(log(guess)/log(2))));
-	return ((maxOps) / ( floor(pow(guess, PWR)) * floor((log(guess) / log(2)))));
+	return ((maxOps)/(guess * floor(sqrt(guess)) * floor(log(guess)/log(2)) ));
 }
 long long int biSearch(long long int maxOps, long long int low, /*long long int mid,*/ long long int high){
 		long long int mid = low+ ((high - low)/2);		
 			double result = equation(maxOps, mid);
-			printf("low %lld\t mid: %lld,\t high:%lld,\t result is %f \n ", low, mid,high, result);
-			if( result < 1 &&  (equation(maxOps, mid+1) >1)){
+			if( result > 1 &&  (equation(maxOps, mid+1) < 1)){
 				return mid;
 			}
-			else if( result <1){
+			else if( result < 1){
 				return biSearch(maxOps, low,mid);
 			}
 			else{
 				return biSearch(maxOps, mid, high);	
 			}
-		//}
-		return 1;
 }
 int main(){
 	int cases;
@@ -37,43 +34,15 @@ int main(){
 		long long unsigned int maxOps = OPS * seconds;
 		long long int currGuess = 10;
 		while(1){
+			currGuess*=10;
 			long long int result = equation(maxOps, currGuess );	
 			if(result<=1){
-				cout<<"We're breaking at "<<currGuess<<endl;
+
 				break;	
-			}
-			cout<<result<<"and the guess is "<<currGuess<<endl;
-			currGuess*=10;	
+			}	
 		}	
-		cout<<currGuess<<" is the guesserino"<<endl;
-		//biSearch(maxOps, 0, currGuess*10);
-		printf("%f \t %f\n", equation(maxOps, 151320), equation(maxOps, 151321));
-/*
-		bool found = false;
-		long long int low = currGuess/10, mid, high = currGuess, finalito = 0;
-		while(!found){
-			mid =  low + floor((high - low )/2);
-			double current = equation(maxOps, mid);
-			
-			if( equation(maxOps, mid ) >= 1 current  >=1 && equation(maxOps, current + 1) <= 1){
-				finalito = mid;
-				found = true;
-				break;
-				//low = mid;
-				//mid = floor((high - low)/2);
-				
-			} 
-			//desired number is higher
-			else if(current  <= 1){
-				high = mid;
-			}
-			//desired number is lower
-			else if (current >= 1){
-				low = mid;
-			}
-		}
-*/
-		
+		//cout<<biSearch(maxOps, 0, currGuess*10)<<endl;
+		printf("%lld\n", biSearch(maxOps, 0, currGuess*10));	
 	}
 	return  0;
 }
