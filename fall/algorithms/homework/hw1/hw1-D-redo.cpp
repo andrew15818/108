a2a13c94db9a942a97b15845e0c86c7aec262996
Andres Ponce <andreseeponce@gmail.com>
2019-10-01 20:32:45 +0800

    sporadic AC on a sea of WA, i think Im close tho :D

--- fall/algorithms/homework/hw1/hw1-D-redo.cpp
+++ fall/algorithms/homework/hw1/hw1-D-redo.cpp
@@ -4,7 +4,7 @@
 #include<cmath>
 using namespace std;
 int counter = 0;
-void pairs(long long int*, long int, long int, long int, long long int*); //prototype
+long long int pairs(long long int*, long int, long int, long int); //prototype
 long long int gcd(const long long int& n1, const long long int& n2){
 
 	if(n2==0){
@@ -16,9 +16,24 @@ long long int helper(long long int* arr, long  int n)
 {
 	long long int curr=0;
 	for(long long int i=0;i<n;i++){
-		pairs(arr,i,n-1,n,&curr);	
+		curr+=pairs(arr,i,n,n);	
 	}
 	return curr;
+}
+long long int pairs(long long int *arr, long int low, long int high, long int n){
+		long int mid = floor((low+high)/2);
+		//printf("calcing for:\t%lld \t%lld \t%lld\n", low, mid,  high );	
+		if(low == high){ //base case
+			//printf("achieved a base case\n");
+			((arr[low]==1 && arr[high]==1))?counter++:counter;
+			return arr[low];	
+		}
+		
+		if ( gcd(pairs(arr,low,mid,n), pairs(arr,mid+1,high,n) )==1 ){
+			//printf("adding %lld-%lld = %lld\t",n,low,n-low);
+			//printf("added the pair(%lld,%lld)\n",arr[low],arr[high]);
+			return counter+=(n-low-1);
+		}
 }
 /*Divide and Conquer, compare the result of the two different halves*/
 /* this function might require just a bit of tweaking , want to try a function that 
@@ -53,13 +68,8 @@ int main(){
 	for(long long int i=0; i<n; i++){
 		cin>>nums[i];	
 	}
-	//long long int puta = helper(nums,n),putamierda=0;
-	//cout<<puta<<endl;
-	//pairs(nums, 0,n-1, n, &putamierda);
-	printf("%lld\n",helper(nums,n));
-	//printf("with helper: %lld, just pairs: %lld\n", puta,putamierda);
-	
-	//cout<<*puta<<endl;
-	//helper(nums, n-1);
-
+	//printf("%lld\n",pairs(nums,0,n-1,n));
+	pairs(nums,0,n-1,n);
+	cout<<counter<<endl;
+	//printf("%lld\n",helper(nums,n-1));
 }
