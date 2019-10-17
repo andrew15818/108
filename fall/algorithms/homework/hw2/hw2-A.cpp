8c94a63926e95cef263c8921306e3de35d63ed45
andrew15818 <andreseeponce@gmail.com>
2019-10-17 23:22:33 +0000

    close to the monotonic paths problem

--- fall/algorithms/homework/hw2/hw2-A.cpp
+++ fall/algorithms/homework/hw2/hw2-A.cpp
@@ -1,7 +1,46 @@
 #include<iostream>
+#include<cstdio>
+#define INCREASING 0
+#define DECREASING 1
+#define MAX_SIZE 1000
 using namespace std;
-long long int arr[1000][1000];
-void paths();
+long int arr[MAX_SIZE][MAX_SIZE];
+long int paths(long int rows, long int cols, long int curRow, long int curCol, long int mState, long int prevElement, 
+				long int monoCount ,long int maxMono){
+/*row and col nums, current row and col, whether state is increasing/decreasing previous ele in seq., max No. of mono switches*/
+	//base cases
+	if(curRow<0 || curRow >= rows || curCol<0 || curCol >= cols){
+		return 0;
+	}	
+	else if(curRow == rows-1 && curCol == cols-1 && (monoCount > maxMono)){
+		return 0;
+	}
+	else if(curRow == rows-1 && curCol == cols-1 && (monoCount <= maxMono)){
+		printf("arriving with info: prevEl: %lld, monoCount: %lld\n", prevElement, monoCount);
+		return 1;
+	}
+	//printf("still alive and well \n");
+	//checking whether we change the monotone state
+	if(arr[curRow][curCol] > prevElement && mState == DECREASING){
+		mState = INCREASING;
+		monoCount++;
+	}
+	else if(arr[curRow][curCol] < prevElement && mState == INCREASING){
+		mState = DECREASING;
+		monoCount++;
+	}
+	return (paths(rows, cols, curRow, curCol+1, mState, arr[curRow][curCol], monoCount, maxMono) || //item directly to the right
+			paths(rows, cols, curRow+1, curCol+1, mState, arr[curRow][curCol], monoCount, maxMono) || //item diagonally left
+			paths(rows, cols, curRow+1, curCol+1, mState, arr[curRow][curCol], monoCount, maxMono)	   //item to the bottom
+			);
+}
 int main(){
-	return 0;
+	long int dimensions, mLimit;
+	cin>>dimensions>>mLimit;
+	for(int i =0; i<dimensions; i++){
+		for(int j=0; j<dimensions; j++){
+			cin>>arr[i][j];
+		}
+	}
+	printf("%lld\n", paths(dimensions, dimensions, 0, 0, 0, arr[0][0], 0, mLimit));
 }
