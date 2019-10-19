9280e50fed0fa024882f276955cdc92c73c0c4db
Andres Ponce <andreseeponce@gmail.com>
2019-10-19 11:56:54 +0800

    TLE on A, need to add memoization, algorithm proabably correct though

--- fall/algorithms/homework/hw2/hw2-A.cpp
+++ fall/algorithms/homework/hw2/hw2-A.cpp
@@ -9,17 +9,20 @@ long int paths(long int rows, long int cols, long int curRow, long int curCol, l
 				long int monoCount ,long int maxMono){
 /*row and col nums, current row and col, whether state is increasing/decreasing previous ele in seq., max No. of mono switches*/
 	//base cases
-	if(curRow<0 || curRow >= rows || curCol<0 || curCol >= cols){
+	if(curRow<0 || curRow >= rows || curCol<0 || curCol >= cols || monoCount>maxMono){
 		return 0;
 	}	
-	else if(curRow == rows-1 && curCol == cols-1 && (monoCount > maxMono)){
+	//if we're at the last element and the count is greater than the max
+	if(curRow == rows-1 && curCol == cols-1 && (monoCount > maxMono)){
+		//printf("failed arrival with prevEl: %ld, monoCount: %ld\n", prevElement, monoCount);
 		return 0;
 	}
+		//if we're at the last element and found a possible path
 	else if(curRow == rows-1 && curCol == cols-1 && (monoCount <= maxMono)){
-		printf("arriving with info: prevEl: %lld, monoCount: %lld\n", prevElement, monoCount);
+		//printf("arriving with info: prevEl: %ld, monoCount: %ld\n", prevElement, monoCount);
 		return 1;
 	}
-	//printf("still alive and well \n");
+
 	//checking whether we change the monotone state
 	if(arr[curRow][curCol] > prevElement && mState == DECREASING){
 		mState = INCREASING;
@@ -29,9 +32,10 @@ long int paths(long int rows, long int cols, long int curRow, long int curCol, l
 		mState = DECREASING;
 		monoCount++;
 	}
+
+	//return the binary value for a possible path
 	return (paths(rows, cols, curRow, curCol+1, mState, arr[curRow][curCol], monoCount, maxMono) || //item directly to the right
-			paths(rows, cols, curRow+1, curCol+1, mState, arr[curRow][curCol], monoCount, maxMono) || //item diagonally left
-			paths(rows, cols, curRow+1, curCol+1, mState, arr[curRow][curCol], monoCount, maxMono)	   //item to the bottom
+			paths(rows, cols, curRow+1, curCol, mState, arr[curRow][curCol], monoCount, maxMono)	   //item to the bottom
 			);
 }
 int main(){
@@ -42,5 +46,10 @@ int main(){
 			cin>>arr[i][j];
 		}
 	}
-	printf("%lld\n", paths(dimensions, dimensions, 0, 0, 0, arr[0][0], 0, mLimit));
+	//printf("the function when the mstate is first set to increasing\n");
+	//printf("%lld\n", paths(dimensions, dimensions, 0, 0, 0, arr[0][0], 1, mLimit));
+	//printf("the function when  mstate is set to increasing first\n");
+
+	int result  = paths(dimensions, dimensions, 0, 0, 1, arr[0][0], 1, mLimit);
+	(result == 1)?printf("Yes\n"):printf("No\n");
 }
