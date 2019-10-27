cc50b36fd311aa24360dc9c4c81ce01dde021849
Andres Ponce <andreseeponce@gmail.com>
2019-10-27 15:39:42 +0800

    still TLE, but got many correct

--- fall/algorithms/homework/hw2/hw2-A-redo.cpp
+++ fall/algorithms/homework/hw2/hw2-A-redo.cpp
@@ -16,6 +16,7 @@ int main(){
 	for(long long int i =0; i<dimensions; i++){
 		for(long long int j=0; j<dimensions; j++){
 			cin>>arr[i][j];	
+			dp[i][j] = -1;
 		}
 	}
 	//printf("INCREASING %d DECREASING %d\n",INCREASING,DECREASING);
@@ -29,9 +30,36 @@ int main(){
 		dp[0][i] = (state[0][i]==state[0][i-1])?dp[0][i-1]:dp[0][i-1]--;
 	}
 	dp[0][0]=monoCount;
-	for(long long int i=0; i<dimensions; i++){
-		for(long long int j=0; j<dimensions; j++){
+	for(long long int i=1; i<dimensions; i++){
+		for(long long int j=1; j<dimensions; j++){
 			//TODO: figure out how to use the arr, dp, and state arrays together				
+			//maybe process the right and upper elements and then just take the max
+			//for the left element
+			long int leftCount= dp[i][j-1];
+			if((arr[i][j-1] > arr[i][j] && state[i][j-1] == DECREASING)
+				|| (arr[i][j-1] < arr[i][j] && state[i][j-1] == INCREASING)){
+				leftCount--;
+			}	
+		
+			//for the top element
+			long int topCount = dp[i-1][j];	
+			if((arr[i-1][j] > arr[i][j] && state[i-1][j] == DECREASING)
+				|| (arr[i-1][j] <arr[i][j] && state[i-1][j]== INCREASING)){
+				topCount--;	
+			}
+		
+			//choosing which element goes in the dp and state arrays
+			long long int maxState = -1, maxDP = 69;
+			if(leftCount>=topCount){
+				maxState = state[i][-1];	
+				maxDP = leftCount;
+			}
+			else if(leftCount<topCount){
+				maxState = state[i-1][j];
+				maxDP = topCount;
+			}
+			state[i][j] = maxState;
+			dp[i][j] =	maxDP; 
 		}	
 	}
 	for(long int i=0; i<dimensions; i++){
@@ -42,5 +70,13 @@ int main(){
 			}
 		}
 	}
-	
+	printf("\n");	
+	for(long int i=0; i<dimensions; i++){
+		for(long int j=0; j<dimensions; j++){
+			printf("%lld ",dp[i][j]);
+			if(j==dimensions-1){
+				printf("\n");	
+			}
+		}
+	}
 }
--- fall/algorithms/homework/hw2/hw2-A.cpp
+++ fall/algorithms/homework/hw2/hw2-A.cpp
@@ -13,6 +13,7 @@ long int max(long int n1, long int n2){
 }
 long int arr[MAX_SIZE][MAX_SIZE];
 long int sol[MAX_SIZE][MAX_SIZE];
+long int state[MAX_SIZE][MAX_SIZE];
 void printArr(long int dimensions)
 {
 	for(long int i=0; i<dimensions; i++){
@@ -33,31 +34,31 @@ long int paths(long int rows, long int cols, long int curRow, long int curCol, l
 	if(curRow <0 || curRow >= rows || curCol < 0 || curCol >= cols || monoCount==0){
 		return 0;
 	}
-	printf("Now checking %lld %lld %lld\n", curRow, curCol ,monoCount);
-	if(sol[curRow][curCol] == 0){
-		return 0;
-	}
-	else if(curRow==0 && curCol == 0 && monoCount > 0){
-		sol[curRow][curCol] = monoCount;
-		return 1;
-	}
-	//checking increasing monotonicity
+	else if(sol[curRow][curCol] != -1){
+		//printf("Iwas usefule\n");
+		//return sol[curRow][curCol];	
+	}	
 	if(arr[curRow][curCol] > prevElement && mState == INCREASING){
-		monoCount--;
-		mState = DECREASING;
-	}		
-	else if(arr[curRow][curCol] < prevElement && mState == DECREASING){
-		monoCount--;
-		mState = INCREASING;
+		state[curRow][curCol] = DECREASING;
+		monoCount--;		
 	}
-	long int temp = max(
-					paths(rows, cols, curRow-1, curCol, mState, arr[curRow][curCol], monoCount, maxMono),
-					paths(rows, cols, curRow, curCol-1, mState, arr[curRow][curCol], monoCount, maxMono)
-					);
-	sol[curRow][curCol] = monoCount; 
-	printArr(rows);
-	return sol[curRow][curCol];
-	
+	else if(arr[curRow][curCol] < prevElement && mState == DECREASING ){
+		state[curRow][curCol] = INCREASING;
+		monoCount--;	
+	}
+	if(curRow==0 && curCol==0 && monoCount > 0){
+			//printf("getting here with mono: %ld", monoCount);
+			sol[curRow][curCol] = monoCount;
+		return 1; 	
+	}
+	else if(monoCount == 0){
+		//printf("we got a problem\n");
+		return 0;	
+	}
+		sol[curRow][curCol] = max(sol[curRow][curCol], monoCount);
+		return 
+				(paths(rows, cols, curRow-1, curCol, mState, arr[curRow][curCol],monoCount,maxMono)||
+						paths(rows, cols, curRow, curCol-1, mState, arr[curRow][curCol], monoCount, maxMono));	
 }	
 int main(){
 	long int dimensions, mLimit;
@@ -65,18 +66,27 @@ int main(){
 	for(int i =0; i<dimensions; i++){
 		for(int j=0; j<dimensions; j++){
 			cin>>arr[i][j];
-			sol[i][j] = -1;
+			state[i][j] = sol[i][j] = -1;
+	
 		}
 	}
 	//printf("the function when the mstate is first set to increasing\n");
 	//printf("the function when  mstate is set to increasing first\n");
-	printf("%lld\n", paths(dimensions, dimensions, dimensions-1, dimensions-1, DECREASING, arr[0][0], mLimit, mLimit));
-	//printf("%lld\n", paths(dimensions, dimensions, 0, 0, 0, arr[0][0], 1, mLimit));
-	/*
+	//printf("%lld\n", paths(dimensions, dimensions, dimensions-1, dimensions-1, INCREASING, arr[0][0], mLimit, mLimit));
+	long int result = paths(dimensions , dimensions, dimensions-1, dimensions-1, INCREASING, arr[0][0], mLimit, mLimit );
+	(result==1)?printf("Yes\n"):printf("No\n");
+/*	
 	for(int i=0; i<dimensions; i++){
 		for(int j=0; j<dimensions; j++){
 			printf("%lld ", sol[i][j]);
 			if(j==dimensions-1){printf("\n");}
+		}
+	}
+	cout<<endl;	
+	for(int i=0; i<dimensions; i++){
+		for(int j=0; j<dimensions; j++){
+			printf("%ld ", state[i][j]);
+			if(j==dimensions-1){cout<<endl;}
 		}
 	}
 	*/
