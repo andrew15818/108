484856d54539c05f441b03aa1e2551724dfe0efb
Andres Ponce <andreseeponce@gmail.com>
2019-12-07 12:23:47 +0800

    now just implement BFS

--- fall/algorithms/homework/hw3/hw3-A.cpp
+++ fall/algorithms/homework/hw3/hw3-A.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 #include<vector>
-#include<deque>
+#include<utility>
+#include<cstdio>
 using namespace std;
 
 struct Vertex{
-	long int id,color; //the color is to use while we search
+	long long int id,color; //the color is to use while we search
+	long long int distance; //distance from the center
+	vector<pair<long long int , Vertex*> > adj;
 };
-
+/*
 struct Edge{
 	//long long int src, dest, weight;
 	Vertex* src, dst;
 	vector<Edge*> adjList;
 };
-
+*/
 class Graph{
 	protected:
-		long int nodes, edges, src, dst; 			
-		Edge **adjList;
+		long long int nodes, edges, src, dst; 			
+		Vertex* vertices;	
 	public:
 		Graph(long int nodes, long int edges, long int srcID, long int dstID)
 		{
@@ -24,15 +27,37 @@ class Graph{
 			this->edges = edges;
 			this->src = srcID;
 			this->dst = dstID;
-			adjList = new Edge *[this->nodes]; 
-
-			for(long int i =0; i< nodes; i++)
-			{
-					
-			}
+			vertices = new Vertex [this->nodes]; 	
 		}
+		void addEdge(long long int source, long long int dest, long long int weight);
+		//for debugging purposes
+		void printGraphInfo();
+		long long int search(Vertex*);
 };
 
+void Graph::addEdge(long long int source, long long int dest, long long int weight)
+{
+	//printf("\tsourve ID: %lld\t source dest:%lld\t weight:%lld\n", source, dest, weight);	
+	vertices[source].adj.push_back(make_pair(weight, &vertices[dest]));
+	vertices[source].id = source;
+	vertices[dest].id = dest;
+	vertices[dest].distance = -1; //initializing the value	
+	vertices[source].distance = -1;
+
+}
+
+void Graph::printGraphInfo()
+{
+	vector<pair<long long int, Vertex*> >::iterator it;
+	for(int i =0; i<nodes; i++){
+		it = vertices[i].adj.begin();	
+		printf("Vertex %d: ", i);
+		for(it; it!=vertices[i].adj.end(); it++){
+			printf("->%lld,(%lld)", (*it).second->id, (*it).first);
+		}
+		printf("\n");
+	}
+} 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -42,6 +67,12 @@ int main(){
 	for(int i =0; i< testcases; i++){
 		cin>>nodes>>edges>>srcID>>dstID;	
 		Graph graph(nodes, edges, srcID, dstID);
+		long long int srcVertex, dstVertex, weight;
+		for(int j=0; j< edges; j++){
+			cin>>srcVertex>>dstVertex>>weight;
+			graph.addEdge(srcVertex,dstVertex, weight);	
+		}
+		graph.printGraphInfo();
 	}
 	return 0;
 }
