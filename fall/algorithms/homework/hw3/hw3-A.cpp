d6343b73484b70eb33b647fd268d327e0ece9dfd
andrew15818 <andreseeponce@gmail.com>
2019-12-07 23:17:15 +0000

    restarted, need to know how to do min-heaps

--- fall/algorithms/homework/hw3/hw3-A.cpp
+++ fall/algorithms/homework/hw3/hw3-A.cpp
@@ -1,86 +1,47 @@
 #include<iostream>
+#include<queue>
 #include<vector>
-#include<utility>
 #include<cstdio>
-#define MAX_WEIGHT 10000000001 //ten million is the max weight
+#define MAX_WEIGHT 10000000001
 using namespace std;
-
-struct Vertex{
-	long long int id,color; //the color is to use while we search
-	long long int distance; //distance from the center
-	vector<pair<long long int , Vertex*> > adj;
+struct Vertex
+{
+	long long int id, dist; //dist from the source
+	vector<pair<long long int, Vertex*> > adj;
+	Vertex* predecessor = NULL; //the predecessor of vertices along the shortest path
 };
-class Graph{
+class  Graph
+{
 	protected:
-		long long int nodes, edges, src, dst; 			
-		Vertex* vertices;	
+		bool *visited;
+		Vertex* vertices;
+		long long int nodes, edges, source, dest; 
+		priority_queue< pair<long long int, Vertex*> > nodeQueue;
 	public:
-		Graph(long int nodes, long int edges, long int srcID, long int dstID)
+		Graph(long long int nodes, long long int edges, long long int source, long  long int dest)
 		{
 			this->nodes = nodes;
 			this->edges = edges;
-			this->src = srcID;
-			this->dst = dstID;
-			vertices = new Vertex [this->nodes]; 	
+			this->source = source;
+			this->dest = dest;
+
+			visited = new bool[nodes]; //to keep track of cycles
+			vertices = new Vertex[nodes]; //for the adjacency
 		}
-		void init();
-		void addEdge(long long int source, long long int dest, long long int weight);
-		void relax(Vertex& v1, Vertex&v2, long long int weight);
-		//for debugging purposes
-		void printGraphInfo();
-		long long int search(Vertex*);
+		~Graph()
+		{
+			delete[] visited;
+			delete[] vertices;
+		}
+		void addEdge(long long int source, long long int dest, long long int weight)
+		{
+			pair< long long int, Vertex* > tmp = make_pair(weight, &vertices[dest]);
+			vertices[source].adj.push_back(tmp);
+			vertices[source].dist = (this->source == source)?0: MAX_WEIGHT;		
+
+		}
 };
-void Graph::init()
-{
-	for(long long int i=0; i<nodes; i++){
-		vertices[i].distance = MAX_WEIGHT;	
-	}
-	vertices[src].distance = 0;
-}
-void Graph::addEdge(long long int source, long long int dest, long long int weight)
-{
-
-	vertices[source].adj.push_back(make_pair(weight, &vertices[dest]));
-	vertices[source].id = source;
-	//vertices[source].distance = MAX_WEIGHT; //we will relax this distance later
-
-	vertices[dest].id = dest;
-	//vertices[dest].distance = MAX_WEIGHT; //initializing the value	
-}
-void Graph::relax(Vertex& v1, Vertex &v2, long long int weight)
-{
-	if(v2.distance > v1.distance + weight){
-		v2.distance = v1.distance + weight;	
-	}	
-}
-void Graph::printGraphInfo()
-{
-	vector<pair<long long int, Vertex*> >::iterator it;
-	for(int i =0; i<nodes; i++){
-		it = vertices[i].adj.begin();	
-		printf("Vertex %d: ", i);
-		for(it; it!=vertices[i].adj.end(); it++){
-			printf("->%lld,(%lld)", (*it).second->id, (*it).first);
-		}
-		printf("\n");
-	}
-} 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	long int testcases, nodes, edges, srcID, dstID;
-	cin>>testcases;
-	for(int i =0; i< testcases; i++){
-		cin>>nodes>>edges>>srcID>>dstID;	
-		Graph graph(nodes, edges, srcID, dstID);
-		graph.init();
-		long long int srcVertex, dstVertex, weight;
-		for(int j=0; j< edges; j++){
-			cin>>srcVertex>>dstVertex>>weight;
-			graph.addEdge(srcVertex,dstVertex, weight);	
-		}
-		graph.printGraphInfo();
-	}
+	
 	return 0;
 }
