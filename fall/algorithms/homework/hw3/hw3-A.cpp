8bf2fbdc794bbb78041c0c5c67564bc94bf67ad6
Andres Ponce <andreseeponce@gmail.com>
2019-12-18 16:19:02 +0800

    almost there, something small wrong

--- fall/algorithms/homework/hw3/hw3-A.cpp
+++ fall/algorithms/homework/hw3/hw3-A.cpp
@@ -9,8 +9,11 @@ using namespace std;
 struct Vertex
 {
 	long long int id, dist; //dist from the source
+	long int deltaDistance; //basically the weight value from predecessor to current node
+	long long int edgeCount; //how many edges we've traveled through
 	vector<pair<long long int, Vertex*> > adj;
 	Vertex* predecessor = NULL; //the predecessor of vertices along the shortest path
+	Vertex* successor = NULL; //the successor in the graph
 	bool visited = false;
 };
 struct compare
@@ -27,6 +30,7 @@ class  Graph
 		long long int nodes, edges, source, dest; 
 		//priority_queue< pair<long long int, Vertex*> > nodeQueue;
 	public:
+		bool foundShortestPath = false;
 		Graph(long long int nodes, long long int edges, long long int source, long  long int dest)
 		{
 			this->nodes = nodes;
@@ -39,34 +43,32 @@ class  Graph
 		}
 		~Graph()
 		{
-			//delete[] visited;
-			//delete[] vertices;
 		}
 		/*TODO: create the vertices and edges separately, otherwise, not all the vertices will be defined*/
 		void addEdge(long long int source, long long int dest, long long int weight)
 		{
 			pair< long long int, Vertex* > tmp = make_pair(weight, &vertices[dest]);
 			vertices[source].adj.push_back(tmp);
-			vertices[source].id = source;
-			vertices[dest].id = dest;
+			vertices[source].id = source;	
 			vertices[source].dist =  MAX_WEIGHT;		
+			vertices[source].edgeCount=  MAX_WEIGHT;		
 
+			vertices[dest].edgeCount =  MAX_WEIGHT;		
+			vertices[dest].id = dest;
+			vertices[dest].dist = MAX_WEIGHT;
 		}
-		/*
-		static bool compare(const Vertex* v1, const Vertex* v2)
-		{
-			return v1->dist < v2->dist;
-		}
-		*/
 		void relax(Vertex* v1, Vertex* v2, long long int weight )
 		{
-			printf("checking: %lld(%lld) and %lld(%lld) with weight %lld\n", v1->id, v1->dist, v2->id, v2->dist, weight);
-			if(v2->dist > v1->dist + weight){
-				v2->dist = v1->dist + weight;
-				
-				printf("\trelaxing: %lld->dist = %lld + %lld\n", v2->id, v1->dist, weight);
+			//printf("checking: %lld(%lld) and %lld(%lld) with weight %lld\n", v1->id, v1->dist, v2->id, v2->dist, weight);
+			if(v2->dist > v1->dist + weight && v2->edgeCount > v1->edgeCount
+					&& weight >= v1->edgeCount+1){
+
+				v2->dist = v1->dist + weight;	
+				v2->edgeCount = v1->edgeCount + 1;
+				v2->deltaDistance = weight;
 				v2->predecessor = v1;
-			}	
+				v1->successor = v2;
+			}
 		}
 		void search()
 		{
@@ -75,43 +77,46 @@ class  Graph
 
 			nodeQueue.push(&vertices[source]);
 			vertices[source].dist = 0;
-			printf("%lld\n", vertices[source].dist);
+			vertices[source].edgeCount = 0;
+			//printf("%lld\n", vertices[source].dist);
 
 			while( !nodeQueue.empty() ){
 				//make_heap(nodeQueue.begin(), nodeQueue.end(), compare);
 				Vertex* v = nodeQueue.top();
+				v->visited = true;
 				nodeQueue.pop();
 				
 				//cout<<"comparing: "<<v->id<<endl;
 				vector< pair<long long int, Vertex*> >::iterator it = v->adj.begin();
 				for(it; it!= v->adj.end(); it++){
-					//printf("relaxing: %lld %lld with weight: %lld\n", v->id, it->second->id, it->first);
+
 					relax(v, it->second, it->first);	
 					if(it->second->visited == false){
 						nodeQueue.push(it->second);
-						//printf("adding: %lld\n", it->second->id);
 					}
 				}
 				
+				if(v->id == dest){
+					//foundShortestPath = true;
+					return;
+					//printf("There does exist a shortest path\n");	
+				}	
+				
 			}
 		}
-		void printGraph(){
-			vector< pair<long long int, Vertex*> >::iterator it;
-			for(long int i=0; i<nodes; i++){
-				printf("%lld: ", i);
-				it = vertices[i].adj.begin();	
-				for(it; it != vertices[i].adj.end(); it++){
-					printf("-> %lld(%lld)", it->second->id, it->first);
-				}
-				printf("\n");
-			}
-			
-		}
-		void printPredecessor(long long int id)
+		int printPredecessor(long long int id)
 		{
-			Vertex *v = &vertices[id]; 
-			printf("pred of %lld is %lld\n", id, v->predecessor->id);
-			printPredecessor(v->predecessor->id);
+			Vertex* v = &vertices[id];
+			if(v->id == source){
+				printf("%lld ",v->id);	
+				return 1;
+			}
+			else if(v->predecessor == NULL){
+				printf("-1");	
+				return 0;
+			}
+				printPredecessor(v->predecessor->id);
+				printf("%lld ",v->id);			
 		}
 };
 int main(){
@@ -119,7 +124,6 @@ int main(){
 	long long int u, v, weight;
 	cin>>testcases;
 	for(long long int i=0; i<testcases;i++){
-		//cin>>nodes>>edges>>startId>>endId;	
 		scanf("%lld %lld %lld %lld", &nodes, &edges, &startId, &endId);
 		Graph g(nodes, edges, startId, endId);
 		for(long long int j=0; j<edges; j++){
@@ -127,8 +131,8 @@ int main(){
 			g.addEdge(u, v, weight);
 		}
 		g.search();
-		//g.printPredecessor(endId);
-		g.printGraph();
+		g.printPredecessor(endId);
+		printf("\n");
 	}
 	return 0;
 }
