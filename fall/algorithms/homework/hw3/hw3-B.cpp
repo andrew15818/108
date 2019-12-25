01e1e717ce940d3e9cdea4d65b0b9d2acd9a1f57
andrew15818 <andreseeponce@gmail.com>
2019-12-25 02:35:17 +0000

    trying problem e

--- fall/algorithms/homework/hw3/hw3-B.cpp
+++ fall/algorithms/homework/hw3/hw3-B.cpp
@@ -2,12 +2,15 @@
 #include<vector>
 #include<iostream>
 #include<queue>
+#include<math.h>
+#define  MAX_WEIGHT 1000000001
 using namespace std;
 struct Vertex
 {
 	long long int id;
 	long long int powerStationCost; //cost to build a station 
 	long long int x, y; //coords
+	long long int minimum = MAX_WEIGHT;
 };
 class Graph
 {
@@ -29,9 +32,29 @@ class Graph
 			vertices[index].x = x;	
 			vertices[index].y = y;
 		}
+		long long int distance(const Vertex* v1, const Vertex* v2)
+		{
+			return abs(v1->x - v2->x) + abs(v1->y - v2->y);
+		}
+		long long int minCost( long long int n1, long long int n2)
+		{
+			return (n1<=n2)?n1:n2;
+		}
 		void printGraph(){
 			for(int i=0; i< nodes; i++)	{
-				printf("%lld: (%lld, %lld), $%lld\n", i, vertices[i].x, vertices[i].y, vertices[i].powerStationCost);	
+				printf("%lld: (%lld, %lld), $%lld, %lld\n", i, vertices[i].x, vertices[i].y, vertices[i].powerStationCost,
+								vertices[i].minimum);	
+			}
+		}
+		void buildMST()
+		{
+			for(int i=0; i< nodes; i++){
+				Vertex* v1 = &vertices[i];
+				for(int j=0; j< nodes; j++){
+					Vertex* v2 = &vertices[j];
+					long long int nodeDist = distance(v1, v2);
+					v2->minimum = minCost(nodeDist, v2->powerStationCost);
+				}
 			}
 		}
 };
@@ -50,6 +73,7 @@ int main(){
 		cin>>x>>y;	
 		graph.addNode(i, x, y);
 	}
+	graph.buildMST();
 	graph.printGraph();
 	return 0;
 }
---
+++ fall/algorithms/homework/hw3/hw3-E.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct Vertex
+{
+	long long int value;
+	vector<Vertex*> adj;
+};
+Vertex *vertex[100000];
+int main()
+{
+	return 0;
+}
