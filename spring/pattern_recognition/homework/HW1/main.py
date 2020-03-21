8a6ca30aef27d335bf09f2ed038967e7873efab2
andrew15818 <andreseeponce@gmail.com>
2020-03-22 00:04:20 +0800

    imported training data

--- spring/pattern_recognition/homework/HW1/main.py
+++ spring/pattern_recognition/homework/HW1/main.py
@@ -1,3 +1,13 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
+
+#REMEMBER TO FOLLOW PEP8 GUIDELINES
+#import the data from the csv, x and y values
+train_df = pd.read_csv("train_data.csv")
+
+#rows and columns of the training data.
+xtrain = train_df['x_train']
+ytrain = train_df['y_train']
+print(xtrain)
+print("We're making it to the end :D")
