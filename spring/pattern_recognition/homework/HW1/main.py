a658a0ba04fe806e74888f56b693ec9fafd06ad4
Andres Ponce <andreseeponce@gmail.com>
2020-03-22 13:19:36 +0800

    plotting code, about to start loss function

--- spring/pattern_recognition/homework/HW1/main.py
+++ spring/pattern_recognition/homework/HW1/main.py
@@ -2,12 +2,30 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-#REMEMBER TO FOLLOW PEP8 GUIDELINES
+'''
+Homework has to follow PEP8 guidelines, including them here for quick reference: 
+    Naming Conventions:
+        function: all lowercase, use underscore for multiple words
+        variable: same, use underscore; make it descriptive
+        class: each word should start with uppercase, don't use underscore
+        method: all lowercase, use udnerscores for readability.
+        constant: all caps, should use underscores for mult. words       
+'''
 #import the data from the csv, x and y values
 train_df = pd.read_csv("train_data.csv")
 
 #rows and columns of the training data.
 xtrain = train_df['x_train']
 ytrain = train_df['y_train']
-print(xtrain)
+
+'''
+Basically: we can use a function of the form f(x) = b0+b1(x),
+to predict the values for the data points. We first randomize the input
+values, and measure the losss against the actual function values. We should
+see the loss go down after a few 'iterations'. Does iterations mean iterations
+through the whole data set or just individual points from the dataset?
+'''
+#generating and showing the graphs
+plt.plot(xtrain,ytrain,'.')
+plt.show()
 print("We're making it to the end :D")
