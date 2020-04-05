0ffb4dcef863d0e6f4e411f4bbae1d4f4b89b07d
Andres Ponce <andreseeponce@gmail.com>
2020-04-05 15:12:46 +0800

    need to track the shortest path

--- spring/ai/homework/Hw1/main.cpp
+++ spring/ai/homework/Hw1/main.cpp
@@ -9,16 +9,59 @@
 #define A_STAR 4
 #define IDA_STAR 5
 #define BOARD_SIZE 8
+#define CHILD_NO 8
 int board[BOARD_SIZE][BOARD_SIZE];
 
 //these functions return the number of expanded nodes
+struct Node
+{
+	int x, y, child_count =0 ;
+	Node *parent, children[CHILD_NO];
+	Node(int x, int y):x(x), y(y){}
+
+	void add_node(struct Node *node){
+		children[child_count] = node;	
+		child_count++;
+		node->parent = this;
+	}
+};
 inline int is_valid(int x, int y)
 {
 	int tmp = 0;
 	tmp = (x < 0 || y < 0 || y >= BOARD_SIZE || x>=BOARD_SIZE)? 0 : 1;
 	return tmp;
 }
-
+int BFS_search(int startx, int starty, int endx, int endy)
+{
+	Node initial(startx, endy);
+	initial.parent = NULL;
+	std::list<Node> frontier;
+	frontier.push_back(initial);
+	
+	//perform fast lookup of whether we already visited this spot
+	int expanded = 0, graph[BOARD_SIZE][BOARD_SIZE]; //store whether the point has been visited
+	memset(graph, 0, sizeof(graph));
+	
+	while( !frontier.empty() )
+	{
+		Node* current = &frontier.front();		
+		frontier.pop_front();
+		printf("testing: %d %d\n", current->x, current->y);
+		expanded++;
+		if(!is_valid(current->x, current->y))
+		{
+			continue;	
+		}
+		else if(current->x == endx && current->y == endy)	
+		{
+			printf("Found target.\n");
+			return expanded;
+		}
+		current->add_node(Node(current->x, current->y));
+	}
+	return 0;
+}
+/*
 int BFS_search(int startx, int starty, int endx, int endy)
 {
 	//printf("startx: %d\tstarty: %d\tendx: %d\t, endy: %d\n", startx, starty,endx, endy);
@@ -26,7 +69,8 @@ int BFS_search(int startx, int starty, int endx, int endy)
 	memset(graph, 0, sizeof(graph));
 	std::pair<int, int> initial(startx, starty); //store the point coordinates
 
-	std::list<std::pair<int, int>> frontier;
+	std::list<std::pair<int, int> > frontier;
+	Node first(initial.first, initial.second);
 
 	frontier.push_back(initial);
 
@@ -38,19 +82,20 @@ int BFS_search(int startx, int starty, int endx, int endy)
 		frontier.pop_front();
 		expanded++;
 		if(current.first == endx && current.second == endy){
-			printf("found target\n");
+			printf("arrived at (%d, %d). Found target\n",current.first, current.second);
 			break;
 		}
 
 		//continue if we've already expaned this node.
 		if(graph[current.first][current.second] == 1 || !is_valid(current.first, current.second)){ 
-			//printf("here\n");
 			continue;
 		}
-		printf("(%d,%d) is being checked with %d elements in list\n", current.first, current.second, frontier.size());
+		printf("(%d,%d) is being checked with %d elements in list\n", 
+				current.first, current.second, frontier.size());
 
 
 		graph[current.first][current.second] = 1;
+		//next nodes in line
 		frontier.push_back(std::pair<int, int>(current.first+1, current.second+2));
 		frontier.push_back(std::pair<int, int>(current.first+1, current.second-2));
 		frontier.push_back(std::pair<int, int>(current.first-1, current.second+2));
@@ -64,7 +109,7 @@ int BFS_search(int startx, int starty, int endx, int endy)
 	printf("returning %d expanded\n", expanded);
 	return expanded;
 }
-
+*/
 int DFS_search(int startx, int starty, int endx, int endy){
 	return 0;
 }
@@ -127,7 +172,7 @@ int main(int argc, char** argv)
 
 	while(algorithm > 0){
 		//scanf("%d %d %d %d", &algorithm, &startx, &starty, &endx, &endy);
-		std::cin>>startx>>starty>>endx>>endy;
+		std::cin>>algorithm>>startx>>starty>>endx>>endy;
 		printf("bout to search %d %d %d %d\n", startx, starty, endx, endy);
 		search(algorithm, startx, starty, endx, endy);
 	}
