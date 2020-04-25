90a872e7bc9d10d2e1b202ad2ffcbfea5fffe210
andrew15818 <andreseeponce@gmail.com>
2020-04-25 23:45:35 +0800

    find k nearest neighbors to do prediction

--- spring/pattern_recognition/homework/HW2/HW2.py
+++ spring/pattern_recognition/homework/HW2/HW2.py
@@ -9,6 +9,8 @@
 # ## Load data
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 x_train = pd.read_csv("x_train.csv").values
 y_train = pd.read_csv("y_train.csv").values[:,0]
@@ -32,6 +34,7 @@ m1 = np.mean(c1, axis = 0)
 m2 = np.mean(c2, axis = 0)
 
 assert m1.shape == (2,)
+print("shape of m1:  " + str(m1.shape) +"\t shape of m2: " + str(m2.shape))
 print(f"mean vector of class 1: {m1}", f"mean vector of class 2: {m2}")
 
 
@@ -41,12 +44,12 @@ print(f"mean vector of class 1: {m1}", f"mean vector of class 2: {m2}")
 tmp = np.array
 
 #the within-class variance is the difference bw actual value and class mean
-s1 = (c1 - m1) ** 2
-s2 = (c2 - m2) ** 2
+s1 = np.array((c1 - m1)) * np.array((c1 - m1 ))
+
+s2 = np.array((c2 - m2)) * np.array((c2 - m2))
 
 s1 = np.sum(s1, axis = 0)
 s2 = np.sum(s2, axis = 0)
-
 sw = np.vstack((s1,s2))
 assert sw.shape == (2,2)
 print(f"Within-class scatter matrix SW: {sw}")
@@ -68,7 +71,7 @@ print(f"Between-class scatter matrix SB: {sb}")
 ## Your code HERE
 #The Fisher Criterion J(w) is the ratio of the in-class variance to the between-class variance
 w = ((m2 - m1) ** 2)/(s1 ** 2 + s2 ** 2)
-print(str(w) + "\t" + str(w.shape))
+w = np.array([[w[0]], [w[1]]])
 assert w.shape == (2,1)
 print(f" Fisher’s linear discriminant: {w}")
 
@@ -76,10 +79,13 @@ print(f" Fisher’s linear discriminant: {w}")
 
 # ## 5. Project the test data by linear discriminant to get the class prediction by nearest-neighbor rule and calculate the accuracy score 
 # you can use accuracy_score function from sklearn.metric.accuracy_score
-y = x_test * w
-acc = accuracy(y_test, y_pred)
+y_pred = x_test * w.T
+plt.plot(c1, '.', color="blue")
+plt.plot(c2, '.', color="red")
+plt.show()
+#acc = accuracy_score(y_test, y_pred)
 
-print(f"Accuracy of test-set {acc}")
+#print(f"Accuracy of test-set {acc}")
 
 
 # ## 6. Plot the 1) projection line 2) Decision boundary and colorize the data with each class
