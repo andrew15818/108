e84ca6c73a7e3a131285fe21ead6040ab5e34f54
Andres Ponce <andreseeponce@gmail.com>
2020-06-06 15:11:06 +0800

    have to fit the regression data using SVR

--- spring/pattern_recognition/homework/HW4/0616110_HW4.py
+++ spring/pattern_recognition/homework/HW4/0616110_HW4.py
@@ -23,7 +23,48 @@ def performance_measure(data):
     for value in data:
         sum += value
     return (sum / len(data))
+
+# Find best C and gamma for either classification or regression
+def find_best_parameters(x_data, y_data, type="classification"):
+    if type == "classification":
+        clf = SVC(C=1.0, kernel='rbf', gamma=0.01) 
+    else: 
+        SVR(C=1.0, kernel='rbf', gamma=0.01)
     
+    best_C = 0
+    best_performance = 0
+    best_gamma = 0
+    for c_test in C:
+        for gamma_test in Gamma:
+                clf = SVC(C=c_test, gamma=gamma_test)
+                clf.fit(x_train, y_train)
+
+                # keep track of the performance of each fold
+                fold_performance = [] 
+                # get the performance using c and gamma of every fold, choose one with lowest MSE
+                for row in range(len(kfold_data)):
+
+                    # the class labels of the elements in validation set
+                    class_indices = y_train[kfold_data[row][1]]
+
+                    # getting the testing set data
+                    testing_fold = x_train[kfold_data[row][1]]
+
+                    # getting class predictions for given set
+                    validation_predictions = clf.predict(testing_fold)
+                    accuracy = accuracy_score(class_indices, validation_predictions) 
+                    fold_performance.append(accuracy)
+
+                    # print(f"\taccuracy for C={c_test} and gamma={gamma_test}\t{accuracy}")
+
+                performance = performance_measure(fold_performance)
+                # print(f"perfomrance for ({c_test}, {gamma_test}): {performance}")
+
+                if performance >= best_performance:
+                    best_performance, best_C, best_gamma = performance, c_test, gamma_test
+
+
+    return (best_performance, best_C, best_gamma) 
 
 # ## Question 1
 # 1. First we split the dataset into K parts
@@ -72,45 +113,13 @@ assert kfold_data[0][1].shape[0] == 4 # The number of data in each validation fo
 # ## Question 2
 # Using sklearn.svm.SVC to train a classifier on the provided train set and conduct the grid search of “C”, “kernel” and “gamma” to find the best parameters by cross-validation.
 
-clf = SVC(C=1.0, kernel='rbf', gamma=0.01)
+
 # Have to search for best combination of parameters
 C = [0.1, 1, 10] 
 Gamma = [0.01, 0.1, 1, 10]
 overall_accuracy = []
 # testing all the parameters of C and gamma and getting the ones that give a best fit
-best_C = 0
-best_performance = 0
-best_gamma = 0
-for c_test in C:
-    for gamma_test in Gamma:
-            clf = SVC(C=c_test, gamma=gamma_test)
-            clf.fit(x_train, y_train)
-
-            # keep track of the performance of each fold
-            fold_performance = [] 
-            # get the performance using c and gamma of every fold, choose one with lowest MSE
-            for row in range(len(kfold_data)):
-
-                # the class labels of the elements in validation set
-                class_indices = y_train[kfold_data[row][1]]
-
-                # getting the testing set data
-                testing_fold = x_train[kfold_data[row][1]]
-
-                # getting class predictions for given set
-                validation_predictions = clf.predict(testing_fold)
-                accuracy = accuracy_score(class_indices, validation_predictions) 
-                fold_performance.append(accuracy)
-
-                # print(f"\taccuracy for C={c_test} and gamma={gamma_test}\t{accuracy}")
-
-            performance = performance_measure(fold_performance)
-            # print(f"perfomrance for ({c_test}, {gamma_test}): {performance}")
-
-            if performance >= best_performance:
-                best_performance, best_C, best_gamma = performance, c_test, gamma_test
-
-best_parameters = [best_performance, best_C, best_gamma]
+best_parameters = find_best_parameters(x_train, y_train, type="classification")
 
 print(best_parameters)
 
@@ -119,13 +128,13 @@ print(best_parameters)
 # Plot the grid search results of your SVM. The x, y represents the hyperparameters of “gamma” and “C”, respectively. And the color represents the average score of validation folds
 # You reults should be look like the reference image ![image](https://miro.medium.com/max/1296/1*wGWTup9r4cVytB5MOnsjdQ.png) 
 # TODO: figure out how to map the table with the different hyper parameters
-'''
+"""
 table = plt.table(
             colLabels=C,
             rowLabels=gamma
         )
 plt.show()
-'''
+"""
 def mock_predict(C, gamma):
     for c in C:
         for g in gamma:
@@ -150,6 +159,8 @@ print("Accuracy score: ", accuracy_score(y_pred, y_test))
 train_df = pd.read_csv("../HW1/train_data.csv")
 x_train = train_df['x_train'].to_numpy().reshape(-1,1)
 y_train = train_df['y_train'].to_numpy().reshape(-1,1)
+print(x_train)
+clf = find_best_parameters(x_train, y_train, type="regression")
 
 test_df = pd.read_csv("../HW1/test_data.csv")
 x_test = test_df['x_test'].to_numpy().reshape(-1,1)
