af255fd924101c734363e8e6f7e5afce8571092e
andrew15818 <andreseeponce@gmail.com>
2020-06-14 00:48:27 +0800

    implemented line checking function

--- spring/ai/homework/Final/test/reversi.py
+++ spring/ai/homework/Final/test/reversi.py
@@ -1,17 +1,17 @@
 BOARD_ROWS = 8
 BOARD_COLS = 8
-board = [[-1, 0, 0, 0, 0, 0, 0, -1],
+board = [[-1, 0, 0, 2, 0, 0, 0, -1],
         [0, 0, 0, 1, 0, 0, 0, 0],
         [0, 0, 0, 1, 0, 0, 0, 0],
         [0, 0, 0, 1, 0, 0, 0, 0], 
-        [0, 0, 1, 2, 1, 1, 0, 0], 
+        [0, 0, 1, 2, 1, 2, 0, 0], 
         [0, 0, 0, 1, 0, 0, 0, 0], 
-        [-1, 0, 0, 0, 0, 0, 0, -1],
-        [0, 0, 0, 0, 0, 0, 0, 0]]
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [-1, 0, 0, 0, 0, 0, 0, -1]]
 
 tiles =  {
     "corner": -1,
-    "unoccupied":0,
+    "unoccupied": 0,
     "white": 1,
     "black": 2
 }
@@ -32,13 +32,43 @@ def is_valid(row, col):
         if col == row:
             return False
     return True 
+# Given the direction of an opponent, check if there is a friendly piece at the end of the 
+# sequence of opponent pieces
+def check_opponent_line(row, col, rowdir, coldir, is_black=False):
+    if not is_valid(row, col):
+        return 0
 
+    # How many enemy pieces lie along this line
+    can_move =  0
+    opponent_team = tiles["black"] if board[row][col]  == tiles["white"] else tiles["white"]
+    player_team = tiles["white"] if board[row][col] == tiles["white"] else tiles["black"]
+
+    while True:
+        # Traveling along the direction
+        row += rowdir
+        col += coldir
+        print(f"checking ({row}, {col})")
+        # Out of bounds
+        if not is_valid(row, col) or board[row][col] == tiles["unoccupied"]:
+            break
+         
+        # If there is another opponent along the direction line
+        elif board[row][col] == opponent_team:
+            can_move += 1
+
+        # If reached same-colored tile at the end of the line
+        elif board[row][col] == player_team:
+            return can_move 
+
+    # If we reach the end, then can't change pieces along this route
+    return 0
 # Get the amount of nodes we can turn by selecting this node
+# TODO: May have to severely change this heuristic function
 def heuristic(node):
     
     opponent = None
     x,y = node[0], node[1]
-    print(f"Calling the heuristic on ({node[0]}, {node[1]})")
+    print(f"\t\tCalling the heuristic on ({node[0]}, {node[1]})")
     if board[x][y] == tiles["black"]:
         opponent = tiles["white"]
     elif board[x][y] == tiles["white"]:
@@ -58,17 +88,19 @@ def heuristic(node):
                         and board[row][col] == opponent):
                         row += i
                         col += j
-                        print(f"\tChecking: ({row},{col})")
+                        #print(f"\tChecking: ({row},{col})")
                         can_turn += 1  
 
 
                     print(f"\tcan turn: {can_turn}")
     return can_turn
 
-def minmax(node, depth, is_maximizing, currsum=0):
+def minmax(node, depth=2, is_maximizing=True, currsum=0):
     # End of the recursion
     if depth == 0:
-        return heuristic(node)
+        value = heuristic(node)
+        print(f"\theuristic for ({node[0]},{node[1]}):{value}")
+        return value
 
     val = -1000 if is_maximizing else 1000
 
@@ -85,19 +117,24 @@ def minmax(node, depth, is_maximizing, currsum=0):
                if is_maximizing:
                    #val = max(val, minmax((x, y), depth-1, False))
                    currsum += minmax((x, y), depth-1, False, currsum=currsum)
-                   print(f"Maximum value: {val}")
+                   print(f"\tMaximum value: {currsum}")
 
                elif not is_maximizing:
                    tmp = minmax((x, y), depth-1, True, currsum=currsum)
                    currsum -= tmp
-                   print(f"Minimum value: {val}")
+                   print(f"\tMinimum value: {currsum}")
             
 
     return currsum
 def GetStep():
     pass
-
+'''
 for row in range(len(board) - 1):
     for col in range(len(board) - 1):
+        if board[row][col] == tiles["unoccupied"]:
+            continue
         print(f"Minmax value for {row}, {col}: {minmax((row, col), 2, False)}")
-#print(heuristic((4, 3)))
+ '''
+#print(heuristic((4,3)))
+#print(minmax( (4, 3), depth=2, is_maximizing=True, currsum=0))
+print(check_opponent_line(4, 3, -1, 0))
