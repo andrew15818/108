6a7144045df837798507081b3973113be3cb95c0
Andres Ponce <andreseeponce@gmail.com>
2020-06-15 02:54:09 +0800

    basic greedy solver

--- spring/ai/homework/Final/test/reversi.py
+++ spring/ai/homework/Final/test/reversi.py
@@ -1,13 +1,20 @@
-BOARD_ROWS = 8
-BOARD_COLS = 8
+
 board = [[-1, 0, 0, 2, 0, 0, 0, -1],
         [0, 0, 0, 1, 0, 0, 0, 0],
         [0, 0, 0, 1, 0, 0, 0, 0],
         [0, 0, 0, 1, 0, 0, 0, 0], 
         [0, 0, 1, 2, 1, 2, 0, 0], 
         [0, 0, 0, 1, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0],
         [-1, 0, 0, 0, 0, 0, 0, -1]]
+'''
+board = [[0, 0, 0],
+        [ 2, 1, 2],
+        [ 0, 1, 1],
+        [ 0, 0, 2]]
+'''
+BOARD_ROWS = len(board)
+BOARD_COLS = len(board[0])
 
 tiles =  {
     "corner": -1,
@@ -47,7 +54,7 @@ def check_opponent_line(row, col, rowdir, coldir, is_black=False):
         # Traveling along the direction
         row += rowdir
         col += coldir
-        print(f"checking ({row}, {col})")
+        print(f"\tchecking ({row}, {col})")
         # Out of bounds
         if not is_valid(row, col) or board[row][col] == tiles["unoccupied"]:
             break
@@ -65,36 +72,23 @@ def check_opponent_line(row, col, rowdir, coldir, is_black=False):
 # Get the amount of nodes we can turn by selecting this node
 # TODO: May have to severely change this heuristic function
 def heuristic(node):
-    
-    opponent = None
-    x,y = node[0], node[1]
-    print(f"\t\tCalling the heuristic on ({node[0]}, {node[1]})")
-    if board[x][y] == tiles["black"]:
-        opponent = tiles["white"]
-    elif board[x][y] == tiles["white"]:
-        opponent = tiles["black"]
 
-    # Amount of chips we can turn color 
-    can_turn = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            row = node[0] + i
-            col = node[1] + j
-            
-            # Check if adjacent tile is of different color
-            if is_valid(row, col): 
-                if board[row][col] == opponent:
-                    while (is_valid(row, col) 
-                        and board[row][col] == opponent):
-                        row += i
-                        col += j
-                        #print(f"\tChecking: ({row},{col})")
-                        can_turn += 1  
+    # How many nodes we can turn starting from this node in total 
+    can_eat = 0
+    for row_dir in range(-1, 2):
+        for col_dir in range(-1, 2):
+            # Out of bounds or repeating same node
+            if row_dir == 0 and col_dir == 0:
+                continue
+            x = node[0] + row_dir
+            y = node[1] + col_dir
+            if not is_valid(x, y):
+                continue
 
-
-                    print(f"\tcan turn: {can_turn}")
-    return can_turn
-
+            can_eat += check_opponent_line(node[0], node[1], row_dir, col_dir)
+    return can_eat
+'''
+# Simple greedy algo to get the best piece             
 def minmax(node, depth=2, is_maximizing=True, currsum=0):
     # End of the recursion
     if depth == 0:
@@ -103,7 +97,6 @@ def minmax(node, depth=2, is_maximizing=True, currsum=0):
         return value
 
     val = -1000 if is_maximizing else 1000
-
 
     # Recursing on the min/max values for all the curr node's children
     for i in range(-1, 2):
@@ -126,15 +119,29 @@ def minmax(node, depth=2, is_maximizing=True, currsum=0):
             
 
     return currsum
-def GetStep():
-    pass
 '''
-for row in range(len(board) - 1):
-    for col in range(len(board) - 1):
-        if board[row][col] == tiles["unoccupied"]:
-            continue
-        print(f"Minmax value for {row}, {col}: {minmax((row, col), 2, False)}")
- '''
-#print(heuristic((4,3)))
-#print(minmax( (4, 3), depth=2, is_maximizing=True, currsum=0))
-print(check_opponent_line(4, 3, -1, 0))
+def GetStep(board, is_black=False):
+
+    best_row = 0 
+    best_col = 0
+    curr_best_heur = -1
+
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+
+            if not is_valid(row, col):
+                continue
+            elif board[row][col] == tiles["corner"]:
+                continue
+
+            tmp = heuristic((row, col))
+            if tmp > curr_best_heur:
+                print(f"Found a new best path through: ({row},{col}): {tmp}")
+                curr_best_heur = tmp
+                best_row = row
+                best_col = col
+
+
+    return (best_row, best_col) 
+
+print(GetStep(board, False))
