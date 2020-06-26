7302000dcc48a2f9abdf4bca8ac60fe07bb86266
Andres Ponce <andreseeponce@gmail.com>
2020-06-27 00:19:53 +0800

    todo: fix index error

--- spring/ai/homework/Hw4/main.py
+++ spring/ai/homework/Hw4/main.py
@@ -29,8 +29,8 @@ def Gini(dataset):
     for label in classes:
         tmp = sum(dataset["class"] == label)
         class_prob += ((tmp) / len(dataset)) ** 2
-        print(class_prob)
-    print(1 - class_prob) 
+        # print(class_prob)
+    # print(1 - class_prob) 
     return 1 - class_prob
 
 # Split the elements into two datasets given a threshold, return split purity
@@ -40,25 +40,39 @@ def split_purity_given_thresh(dataset, attribute, threshold):
     lower_than = dataset[dataset[attribute] <= threshold]
     greater_than = dataset[dataset[attribute] > threshold]
 
-    Gini(lower_than)
-    Gini(greater_than) 
-    pass 
+    return (Gini(lower_than) + Gini(greater_than))
+    
 
 # Build the tree according to best features given a dataset
 def build_tree(data):
     node = Node()
+    best_gini = 1.0
+    best_right, best_left = None, None
+    best_feature = ""
     for attr in attributes:
         # Getting the column of every attribute in the dataset
         feature_col = data[attr].copy()
 
         # TODO: Make sure the values are being sorted correctly
         feature_col.sort_values(ascending=True) 
-        for value in range(len(feature_col) - 1):
+        for value in range(0, len(feature_col) - 1):
 
             # Threshold is the average of two adjacent sorted values
            thresh = (feature_col[value] + feature_col[value + 1]) / 2
 
-           split_purity_given_thresh(data, attr, thresh)
-
-
+           split_gini = split_purity_given_thresh(data, attr, thresh)
+           if split_gini < best_gini:
+                best_feature = attr
+                best_right = data[data[attr] <= thresh]
+                best_left  = data[data[attr] > thresh]
+                best_gini = split_gini
+                print(f"\tNew best_gini: {best_gini}") 
+    node.feature_name = attr
+    node.threshold = thresh
+    print(f"RECURSING with right data{best_right.shape}\tbest left: {best_left.shape}")
+    node.left = build_tree(best_left)
+    node.right = build_tree(best_right)
+    
+    print(f"best gini: {best_gini}")
+    return node
 build_tree(x_data)
