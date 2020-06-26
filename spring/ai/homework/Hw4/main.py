7a1841c813dd4c1ece6156ec68058a1b1c684137
Andres Ponce <andreseeponce@gmail.com>
2020-06-26 17:03:13 +0800

    verify the gini function

---
+++ spring/ai/homework/Hw4/main.py
@@ -0,0 +1,64 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+# Load the dataset
+x_data = pd.read_csv("iris.data")
+
+attributes = ["sepal length", "sepal width", 
+                "petal length", "petal width"]
+
+classes = x_data["class"].unique()
+
+print(classes)
+class Node:
+    def __init__(self):
+        feature_name = ""
+        threshold = 0
+        # Children
+        right = None
+        Left = None
+
+print(f"Elements in the dataset: {len(x_data)}")
+
+# Splitting the datasets into training and validation
+x_train, x_test, y_train, y_test = train_test_split(x_data, x_data["class"], test_size=0.2)
+
+# Get the Gini impurity by seeing how many elements are in each set
+def Gini(dataset):
+    class_prob = 0
+    for label in classes:
+        tmp = sum(dataset["class"] == label)
+        class_prob += ((tmp) / len(dataset)) ** 2
+        print(class_prob)
+    print(1 - class_prob) 
+    return 1 - class_prob
+
+# Split the elements into two datasets given a threshold, return split purity
+def split_purity_given_thresh(dataset, attribute, threshold):
+    # TODO: Find if there is a way to do the split using less columns
+    # Split the datasets according to the threshold
+    lower_than = dataset[dataset[attribute] <= threshold]
+    greater_than = dataset[dataset[attribute] > threshold]
+
+    Gini(lower_than)
+    Gini(greater_than) 
+    pass 
+
+# Build the tree according to best features given a dataset
+def build_tree(data):
+    node = Node()
+    for attr in attributes:
+        # Getting the column of every attribute in the dataset
+        feature_col = data[attr].copy()
+
+        # TODO: Make sure the values are being sorted correctly
+        feature_col.sort_values(ascending=True) 
+        for value in range(len(feature_col) - 1):
+
+            # Threshold is the average of two adjacent sorted values
+           thresh = (feature_col[value] + feature_col[value + 1]) / 2
+
+           split_purity_given_thresh(data, attr, thresh)
+
+
+build_tree(x_data)
